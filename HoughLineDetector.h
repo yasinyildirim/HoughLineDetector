d1292e38fc12fd0956651adfc826d3227db74281
yasinyildirim <yildirimyasi@gmail.com>
2017-12-01 20:09:59 +0300

    initial commit

--- HoughLineDetector.h
+++ HoughLineDetector.h
@@ -1,8 +1,16 @@
-/***
-
-
-
-*/
+/*#******************************************************************************
+ ** IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
+ **
+ ** By downloading, copying, installing or using the software you agree to this license (LGPL).
+ ** If you do not agree to this license, do not download, install,
+ ** copy or use the software.
+ **
+ ** See COPYING file for license information.
+ **
+ **  Creation - November 2017
+ **      Author: Yasin Yıldırım (yildirimyasi(at)gmail(dot)com), Istanbul, Turkey
+ **
+*******************************************************************************/
 #pragma once
 #include <vector>
 #include <opencv2\core.hpp>
